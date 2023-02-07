7e3671d3a3027e3c69d5f02f25a60802dd0ff023
付志强 <zhiqiangfu6@gmail.com>
2023-02-07 23:22:03 +0800

    feat: card

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,6 +3,8 @@ import Test1 from "./ProTest/Test1";
 import ComponentsTest from "./ProTest/Test2";
 import Test3 from "./ProTest/Test3";
 import Test4 from "./ProTest/Test4";
+import Test5 from "./ProTest/Test5";
+import Test6 from "./ProTest/Test6";
 
 export default function App() {
   return (
@@ -10,7 +12,9 @@ export default function App() {
       {/* <Test1 /> */}
       {/* <ComponentsTest /> */}
       {/* <Test3 /> */}
-      <Test4 />
+      {/* <Test4 /> */}
+      {/* <Test5 /> */}
+      <Test6 />
     </div>
   );
 }
--- example/src/ProTest/Test1.tsx
+++ example/src/ProTest/Test1.tsx
@@ -1,3 +1,4 @@
+import { WaterMark } from "@ant-design/pro-components";
 import {
   ProForm,
   ProFormDatePicker,
@@ -8,15 +9,17 @@ import React from "react";
 export default function Test1() {
   return (
     <div>
-      <ProForm
-        onFinish={async (values) => {
-          console.log(values);
-        }}
-      >
-        <ProFormText name="name" label="姓名" />
-        <ProFormText name="age" label="年龄" />
-        <ProFormDatePicker name="date" label="请选择日期" />
-      </ProForm>
+      <WaterMark content="蚂蚁集团">
+        <ProForm
+          onFinish={async (values) => {
+            console.log(values);
+          }}
+        >
+          <ProFormText name="name" label="姓名" />
+          <ProFormText name="age" label="年龄" />
+          <ProFormDatePicker name="date" label="请选择日期" />
+        </ProForm>
+      </WaterMark>
     </div>
   );
 }
