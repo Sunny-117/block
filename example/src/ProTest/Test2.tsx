cd3aeac2b8d1d66bbd406bb789bb14df3cfe1314
付志强 <zhiqiangfu6@gmail.com>
2023-02-07 22:27:34 +0800

    fix(example): test2 finish

--- example/src/ProTest/Test2.tsx
+++ example/src/ProTest/Test2.tsx
@@ -85,130 +85,6 @@ const List: React.FC<{ title: string; style?: React.CSSProperties }> = (
   );
 };
 
-const MenuCard = () => {
-  const { token } = theme.useToken();
-  return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <Divider
-        style={{
-          height: "1.5em",
-        }}
-        type="vertical"
-      />
-      <Popover
-        placement="bottom"
-        overlayStyle={{
-          width: "calc(100vw - 24px)",
-          padding: "24px",
-          paddingTop: 8,
-          height: "307px",
-          borderRadius: "0 0 6px 6px",
-        }}
-        content={
-          <div style={{ display: "flex", padding: "32px 40px" }}>
-            <div style={{ flex: 1 }}>
-              <List title="金融解决方案" />
-              <List
-                title="其他解决方案"
-                style={{
-                  marginBlockStart: 32,
-                }}
-              />
-            </div>
-
-            <div
-              style={{
-                width: "308px",
-                borderInlineStart: "1px solid " + token.colorBorder,
-                paddingInlineStart: 16,
-              }}
-            >
-              <div
-                className={css`
-                  font-size: 14px;
-                  color: ${token.colorText};
-                  line-height: 22px;
-                `}
-              >
-                热门产品
-              </div>
-              {new Array(3).fill(1).map((name, index) => {
-                return (
-                  <div
-                    key={index}
-                    className={css`
-                      border-radius: 4px;
-                      padding: 16px;
-                      margin-top: 4px;
-                      display: flex;
-                      cursor: pointer;
-                      &:hover {
-                        background-color: ${token.colorBgTextHover};
-                      }
-                    `}
-                  >
-                    <img src="https://gw.alipayobjects.com/zos/antfincdn/6FTGmLLmN/bianzu%25252013.svg" />
-                    <div
-                      style={{
-                        marginInlineStart: 14,
-                      }}
-                    >
-                      <div
-                        className={css`
-                          font-size: 14px;
-                          color: ${token.colorText};
-                          line-height: 22px;
-                        `}
-                      >
-                        Ant Design
-                      </div>
-                      <div
-                        className={css`
-                          font-size: 12px;
-                          color: ${token.colorTextSecondary};
-                          line-height: 20px;
-                        `}
-                      >
-                        杭州市较知名的 UI 设计语言
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        }
-      >
-        <div
-          style={{
-            color: token.colorTextHeading,
-            fontWeight: 500,
-            cursor: "pointer",
-            display: "flex",
-            gap: 4,
-            paddingInlineStart: 8,
-            paddingInlineEnd: 12,
-            alignItems: "center",
-          }}
-          className={css`
-            &:hover {
-              background-color: ${token.colorBgTextHover};
-            }
-          `}
-        >
-          <span> 企业级资产中心</span>
-          <CaretDownFilled />
-        </div>
-      </Popover>
-    </div>
-  );
-};
-
 const SearchInput = () => {
   const { token } = theme.useToken();
   return (
@@ -269,84 +145,10 @@ const ComponentsTest = () => {
     >
       <ProConfigProvider hashed={false}>
         <ProLayout
-          prefixCls="my-prefix"
-          bgLayoutImgList={[
-            {
-              src: "https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png",
-              left: 85,
-              bottom: 100,
-              height: "303px",
-            },
-            {
-              src: "https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png",
-              bottom: -68,
-              right: -45,
-              height: "303px",
-            },
-            {
-              src: "https://img.alicdn.com/imgextra/i3/O1CN018NxReL1shX85Yz6Cx_!!6000000005798-2-tps-884-496.png",
-              bottom: 0,
-              left: 0,
-              width: "331px",
-            },
-          ]}
           {...defaultProps}
           location={{
             pathname,
           }}
-          siderMenuType="group"
-          menu={{
-            collapsedShowGroupTitle: true,
-          }}
-          avatarProps={{
-            src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
-            size: "small",
-            title: "七妮妮",
-          }}
-          actionsRender={(props) => {
-            if (props.isMobile) return [];
-            return [
-              props.layout !== "side" && document.body.clientWidth > 1400 ? (
-                <SearchInput />
-              ) : undefined,
-              <InfoCircleFilled key="InfoCircleFilled" />,
-              <QuestionCircleFilled key="QuestionCircleFilled" />,
-              <GithubFilled key="GithubFilled" />,
-            ];
-          }}
-          headerTitleRender={(logo, title, _) => {
-            const defaultDom = (
-              <a>
-                {logo}
-                {title}
-              </a>
-            );
-            if (document.body.clientWidth < 1400) {
-              return defaultDom;
-            }
-            if (_.isMobile) return defaultDom;
-            return (
-              <>
-                {defaultDom}
-                <MenuCard />
-              </>
-            );
-          }}
-          menuFooterRender={(props) => {
-            if (props?.collapsed) return undefined;
-            return (
-              <div
-                style={{
-                  textAlign: "center",
-                  paddingBlockStart: 12,
-                }}
-              >
-                <div>© 2021 Made with love</div>
-                <div>by Ant Design</div>
-              </div>
-            );
-          }}
-          onMenuHeaderClick={(e) => console.log(e)}
           menuItemRender={(item, dom) => (
             <div
               onClick={() => {
@@ -358,47 +160,16 @@ const ComponentsTest = () => {
           )}
           {...settings}
         >
-          <PageContainer
-            token={{
-              paddingInlinePageContainerContent: num,
-            }}
-            extra={[
-              <Button key="3">操作</Button>,
-              <Button key="2">操作</Button>,
-              <Button
-                key="1"
-                type="primary"
-                onClick={() => {
-                  setNum(num > 0 ? 0 : 40);
-                }}
-              >
-                主操作
-              </Button>,
-            ]}
-            subTitle="简单的描述"
-            footer={[
-              <Button key="3">重置</Button>,
-              <Button key="2" type="primary">
-                提交
-              </Button>,
-            ]}
-          >
-            <ProCard
-              style={{
-                height: "200vh",
-                minHeight: 800,
-              }}
-            >
-              <div />
-            </ProCard>
+          <PageContainer>
+            <h1>demo</h1>
           </PageContainer>
 
           <SettingDrawer
             pathname={pathname}
             enableDarkTheme
-            getContainer={() => document.getElementById("test-pro-layout")}
+            getContainer={() => document.getElementById("demo")}
             settings={settings}
-            onSettingChange={(changeSetting) => {
+            onSettingChange={(changeSetting: any) => {
               setSetting(changeSetting);
             }}
             disableUrlParams={false}
